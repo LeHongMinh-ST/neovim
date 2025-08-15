69c489cab0435338a0913dbd948a45b2dbe889d9
minhlh <minhlh@haposoft.com>
2025-08-15 11:10:06 +0700

    add format

--- lua/plugins/comform.lua
+++ lua/plugins/comform.lua
@@ -3,12 +3,29 @@ return {
   opts = {
     formatters_by_ft = {
       php = { "php_cs_fixer" },
+      javascript = { "biome" },
+      typescript = { "biome" },
+      javascriptreact = { "biome" },
+      typescriptreact = { "biome" },
+      json = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
     },
     formatters = {
       php_cs_fixer = {
         command = "php-cs-fixer",
         args = { "fix", "--using-cache=no", "$FILENAME" },
         stdin = false,
+      },
+      prettier = {
+        command = "prettier",
+        args = { "--stdin-filepath", "$FILENAME" },
+        stdin = true,
+      },
+      biome = {
+        command = "biome",
+        args = { "format", "--stdin-file-path", "$FILENAME" },
+        stdin = true,
       },
     },
   },
